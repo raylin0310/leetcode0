32735a216bb50fde0b51f31ac5e361fcfc166842
lilin <lilin-2@thunisoft.com>
2020-03-24 19:31:52 +0800

    274、275

---
+++ java/_274_HIndex.java
@@ -0,0 +1,91 @@
+import java.util.Arrays;
+
+/**
+ * _274_HIndex
+ * @author lilin
+ * @date 2020-3-24 14:12
+ */
+public class _274_HIndex {
+	/*
+		Given an array of citations (each citation is a non-negative integer) of a researcher,
+		write a function to compute the researcher's h-index.
+
+		According to the definition of h-index on Wikipedia:
+		"A scientist has index h if h of his/her N papers have at least h citations each,
+		and the other N − h papers have no more than h citations each."
+
+		给定一位研究者论文被引用次数的数组（被引用次数是非负整数）。
+		编写一个方法，计算出研究者的 h 指数。
+
+		h 指数的定义:
+		“h 代表“高引用次数”（high citations），一名科研人员的 h 指数是指他（她）的 （N 篇论文中）至多有 h 篇论文分别被引用了至少 h 次。
+		（其余的 N - h 篇论文每篇被引用次数不多于 h 次。）”
+
+
+		Example:
+
+		Input: citations = [3,0,6,1,5]
+		Output: 3
+		Explanation: [3,0,6,1,5] means the researcher has 5 papers in total and each of them had
+		received 3, 0, 6, 1, 5 citations respectively.
+		Since the researcher has 3 papers with at least 3 citations each and the remaining
+		two with no more than 3 citations each, her h-index is 3.
+		Note: If there are several possible values for h, the maximum one is taken as the h-index.
+
+	*/
+
+	public int hIndex(int[] citations) {
+		int n = citations.length;
+		// hash数组
+		int[] count = new int[n + 1];
+		for (int citation : citations) {
+			count[Math.min(citation, n)] += 1;
+		}
+		System.out.println(Arrays.toString(count));
+		int sum = 0;
+		for (int i = n; i > 0; i--) {
+			sum += count[i];
+			if (sum >= i) {
+				return i;
+			}
+		}
+		return 0;
+	}
+
+	/**          .  (x=y)
+	 * |       .  /
+	 * |        /
+	 * |      /
+	 * |    / .
+	 * |  / .
+	 * |/___________________________________
+	 *
+	 *  https://leetcode-cn.com/problems/h-index/solution/hzhi-shu-by-leetcode/
+	 */
+
+	public int hIndex2(int[] citations) {
+		Arrays.sort(citations);
+		// i表示文章个数
+		int i = 0;
+		while (i < citations.length && citations[citations.length - 1 - i] > i) {
+			i++;
+		}
+		return i;
+	}
+
+	public int hIndex3(int[] citations) {
+		Arrays.sort(citations);
+		int res = citations.length - 1;
+		while (res >= 0 && citations[res] > res) {
+			res--;
+		}
+		// 结果返回的是‘几篇文章’满足要求
+		return citations.length - 1 - res;
+	}
+
+	public static void main(String[] args) {
+		_274_HIndex test = new _274_HIndex();
+		int[] nums = {3, 0, 6, 1, 5};
+		System.out.println(test.hIndex3(nums));
+	}
+}
---
+++ java/_275_HIndexII.java
@@ -0,0 +1,79 @@
+
+/**
+ * _275_HIndexII
+ * @author lilin
+ * @date 2020-3-24 15:02
+ */
+public class _275_HIndexII {
+	/*
+		Given an array of citations sorted in ascending order (each citation is a non-negative integer) of a researcher,
+		write a function to compute the researcher's h-index.
+
+		According to the definition of h-index on Wikipedia:
+		"A scientist has index h if h of his/her N papers have at least h citations each,
+		and the other N − h papers have no more than h citations each."
+
+		给定一位研究者论文被引用次数的数组（被引用次数是非负整数），数组已经按照升序排列。编写一个方法，计算出研究者的 h 指数。
+
+		h 指数的定义: “h 代表“高引用次数”（high citations），
+		一名科研人员的 h 指数是指他（她）的 （N 篇论文中）至多有 h 篇论文分别被引用了至少 h 次。（其余的 N - h 篇论文每篇被引用次数不多于 h 次。）"
+
+
+		Example:
+
+		Input: citations = [0,1,3,5,6]
+		Output: 3
+		Explanation: [0,1,3,5,6] means the researcher has 5 papers in total and each of them had
+		received 0, 1, 3, 5, 6 citations respectively.
+		Since the researcher has 3 papers with at least 3 citations each and the remaining
+		two with no more than 3 citations each, her h-index is 3.
+		Note:
+
+		If there are several possible values for h, the maximum one is taken as the h-index.
+
+		Follow up:
+
+		This is a follow up problem to H-Index, where citations is now guaranteed to be sorted in ascending order.
+		Could you solve it in logarithmic time complexity?
+	*/
+
+	/**
+	 * 与274不同的是，已经排好序
+	 *
+	 */
+
+	public int hIndex(int[] citations) {
+		int n = citations.length;
+		for (int i = 0; i < n; i++) {
+			if (citations[i] >= n - i) {
+				return n - i;
+			}
+		}
+		return 0;
+	}
+
+	public int hIndex3(int[] citations) {
+		int n = citations.length;
+		int left = 0;
+		int right = n - 1;
+		int res = 0;
+		while (left <= right) {
+			int mid = (right - left) / 2 + left;
+			if (citations[mid] >= n - mid) {
+				right = mid - 1;
+				res = n - mid;
+			} else {
+				left = mid + 1;
+			}
+		}
+		return res;
+	}
+
+
+	public static void main(String[] args) {
+		_275_HIndexII test = new _275_HIndexII();
+		int[] nums = {0, 1, 3, 5, 6};
+		int[] nums2 = {0, 1};
+		System.out.println(test.hIndex(nums2));
+	}
+}
