6c206747ccf6ab2080a9a0cc83759a61877c302c
lilin <lilin-2@thunisoft.com>
2020-05-13 20:20:33 +0800

    334、128

---
+++ java/_128_LongestConsecutiveSequence.java
@@ -0,0 +1,92 @@
+/*
+ * @projectName leetcode0
+ * @package PACKAGE_NAME
+ * @className PACKAGE_NAME._128_LongestConsecutiveSequence
+ * @copyright Copyright 2018 Thunisoft, Inc. All rights reserved.
+ */
+
+import java.util.HashMap;
+import java.util.HashSet;
+
+/**
+ * _128_LongestConsecutiveSequence
+ * @author lilin
+ * @date 2020-5-11 18:03
+ */
+public class _128_LongestConsecutiveSequence {
+/*
+	给定一个未排序的整数数组，找出最长连续序列的长度。
+
+	要求算法的时间复杂度为 O(n)。
+
+	示例:
+
+	输入: [100, 4, 200, 1, 3, 2]
+	输出: 4
+	解释: 最长连续序列是 [1, 2, 3, 4]。它的长度为 4。
+
+	*/
+
+	public static int longestConsecutive(int[] nums) {
+		if (nums == null || nums.length == 0) return 0;
+		HashSet<Integer> set = new HashSet<>();
+		int res = 0;
+		for (int num : nums) {
+			set.add(num);
+		}
+		for (int num : nums) {
+			int down = num - 1;
+			while (set.contains(down)) {
+				set.remove(down);
+				down--;
+			}
+			int up = num + 1;
+			while (set.contains(up)) {
+				set.remove(up);
+				up++;
+			}
+			res = Math.max(res, up - down - 1);
+		}
+		return res;
+	}
+
+	/**
+	 * @link https://leetcode.com/problems/longest-consecutive-sequence/discuss/41055/My-really-simple-Java-O(n)-solution-Accepted
+	 */
+
+	public static int longestConsecutive2(int[] num) {
+		int res = 0;
+		HashMap<Integer, Integer> map = new HashMap<>();
+		for (int n : num) {
+			if (!map.containsKey(n)) {
+				int left = (map.containsKey(n - 1)) ? map.get(n - 1) : 0;
+				int right = (map.containsKey(n + 1)) ? map.get(n + 1) : 0;
+				// sum: length of the sequence n is in
+				int sum = left + right + 1;
+				map.put(n, sum);
+
+				// keep track of the max length
+				res = Math.max(res, sum);
+
+				// extend the length to the boundary(s)
+				// of the sequence
+				// will do nothing if n has no neighbors
+				map.put(n - left, sum);
+				map.put(n + right, sum);
+			}
+			else {
+				// duplicates
+				continue;
+			}
+		}
+		return res;
+	}
+
+	public static void main(String[] args) {
+		//int[] nums = {100, 4, 200, 1, 3, 2};
+		//int[] nums = {1, 2, 3, 4, 3, 2,1};
+		int[] nums = {1, 3, 2, 4};
+		System.out.println(longestConsecutive2(nums));
+	}
+
+}
---
+++ java/_334_IncreasingTripletSubsequence.java
@@ -0,0 +1,61 @@
+/*
+ * @projectName leetcode0
+ * @package PACKAGE_NAME
+ * @className PACKAGE_NAME._334_IncreasingTripletSubsequence
+ * @copyright Copyright 2018 Thunisoft, Inc. All rights reserved.
+ */
+
+/**
+ * _334_IncreasingTripletSubsequence
+ * @author lilin
+ * @date 2020-5-11 17:45
+ */
+public class _334_IncreasingTripletSubsequence {
+/*
+	给定一个未排序的数组，判断这个数组中是否存在长度为 3 的递增子序列。
+
+	数学表达式如下:
+
+	如果存在这样的 i, j, k,  且满足 0 ≤ i < j < k ≤ n-1，
+	使得 arr[i] < arr[j] < arr[k] ，返回 true ; 否则返回 false 。
+	说明: 要求算法的时间复杂度为 O(n)，空间复杂度为 O(1) 。
+
+	示例 1:
+
+	输入: [1,2,3,4,5]
+	输出: true
+	示例 2:
+
+	输入: [5,4,3,2,1]
+	输出: false
+	*/
+
+	public static boolean increasingTriplet(int[] nums) {
+		int small = Integer.MAX_VALUE;
+		int mid = Integer.MAX_VALUE;
+		for (int num : nums) {
+			if (num <= small) {
+				small = num;
+			} else if (num < mid) {
+				mid = num;
+			} else if (num > mid) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+	/**
+
+	  1,2,3,7,5
+
+	 */
+
+	public static void main(String[] args) {
+		int[] nums = {3,5,1,2,3};
+		System.out.println(increasingTriplet(nums));
+	}
+
+}
