863e666793912a2f56bacc2117f5eb87e7fbe53c
llin <lilinz@shiqiao.com>
2022-05-26 11:37:20 +0800

    1

--- java/_376_WiggleSubsequence.java
+++ java/_376_WiggleSubsequence.java
@@ -50,6 +50,30 @@ public class _376_WiggleSubsequence {
 		if (nums.length == 0) {
 			return 0;
 		}
+		int n = nums.length;
+		int[] up = new int[n];
+		int[] down = new int[n];
+		up[0] = 1;
+		down[0] = 1;
+		for (int i = 1; i < n; i++) {
+			if (nums[i] > nums[i - 1]) {
+				up[i] = Math.max(up[i - 1], down[i - 1] + 1);
+				down[i] = down[i - 1];
+			} else if (nums[i] < nums[i - 1]) {
+				down[i] = Math.max(down[i - 1], up[i - 1] + 1);
+				up[i] = up[i - 1];
+			} else {
+				up[i] = up[i - 1];
+				down[i] = down[i - 1];
+			}
+		}
+		return Math.max(up[n - 1], down[n - 1]);
+	}
+
+	public static int wiggleMaxLength1(int[] nums) {
+		if (nums.length == 0) {
+			return 0;
+		}
 		int up = 1, down = 1;
 		for (int i = 1; i < nums.length; i++) {
 			if (nums[i] > nums[i - 1]) {
@@ -92,7 +116,7 @@ public class _376_WiggleSubsequence {
 	public static void main(String[] args) {
 		int[] nums = {1, 7, 4, 9, 2, 5};
 		int[] nums1 = {1, 7, 8, 9, 10};
-		int r = wiggleMaxLength(nums1);
-		System.out.println(r);
+		System.out.println(wiggleMaxLength(nums1));
+		System.out.println(wiggleMaxLength1(nums1));
 	}
 }
